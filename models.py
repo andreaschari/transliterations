25709be76d9e30f11c09904335fff181896f3122
Andreas Chari <andreas9810@outlook.com>
2025-04-23 10:54:54 +0100

    add BGE-M3 models hugginface entries

--- models.py
+++ models.py
@@ -1,14 +1,14 @@
 model_paths = {
     "bge-m3": None,
-    "bge-m3-ZH_MMARCO_50": "/root/nfs/CLIR/data/models/bge-m3-ZH_MMARCO_50",
-    "bge-m3-ZH_MMARCO_TRANSLIT": "/root/nfs/CLIR/data/models/bge-m3-tZH_MMARCO",
-    "bge-m3-ZH_MMARCO_NATIVE": "/root/nfs/CLIR/data/models/bge-m3-ZH_MMARCO_NATIVE",
-    "bge-m3-RU_MMARCO_50": "/root/nfs/CLIR/data/models/bge-m3-RU_MMARCO_50",
-    "bge-m3-RU_MMARCO_TRANSLIT": "/root/nfs/CLIR/data/models/bge-m3-RU_MMARCO_TRANSLIT",
-    "bge-m3-RU_MMARCO_NATIVE": "/root/nfs/CLIR/data/models/bge-m3-RU_MMARCO_NATIVE",
-    "bge-m3-ZH_MMARCO_50_SPACES": "/root/nfs/CLIR/data/models/bge-m3-ZH_MMARCO_50_SPACES",
-    "bge-m3-ZH_MMARCO_NATIVE_SPACES": "/root/nfs/CLIR/data/models/bge-m3-ZH_MMARCO_NATIVE_SPACES",
-    "bge-m3-ZH_MMARCO_TRANSLIT_SPACES": "/root/nfs/CLIR/data/models/bge-m3-ZH_MMARCO_TRANSLIT_SPACES",
+    "bge-m3-ZH_MMARCO_50": "andreaschari/bge-m3-ZH_MMARCO_50_MIXED",
+    "bge-m3-ZH_MMARCO_50_SPACES": "andreaschari/bge-m3-ZH_MMARCO_50_MIXED_ANSERINI",
+    "bge-m3-ZH_MMARCO_TRANSLIT": "andreaschari/bge-m3-ZH_MMARCO_TRANSLIT",
+    "bge-m3-ZH_MMARCO_TRANSLIT_SPACES": "andreaschari/bge-m3-ZH_MMARCO_TRANSLIT_ANSERINI",
+    "bge-m3-ZH_MMARCO_NATIVE": "andreaschari/bge-m3-ZH_MMARCO_NATIVE",
+    "bge-m3-ZH_MMARCO_NATIVE_SPACES": "andreaschari/bge-m3-ZH_MMARCO_NATIVE_ANSERINI",
+    "bge-m3-RU_MMARCO_50": "andreaschari/bge-m3-RU_MMARCO_50_MIXED",
+    "bge-m3-RU_MMARCO_TRANSLIT": "andreaschari/bge-m3-RU_MMARCO_TRANSLIT",
+    "bge-m3-RU_MMARCO_NATIVE": "andreaschari/bge-m3-RU_MMARCO_NATIVE",
 }
 
 t5_model_paths = {
